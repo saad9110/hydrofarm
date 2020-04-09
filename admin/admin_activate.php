87a0f6adc06927fd3f18c34f14598440950ec86d
Rahat Murtaza <rahat_murtaza@outlook.com>
2020-04-10 01:06:11 +0500

    Please Add Query

--- admin/admin_activate.php
+++ admin/admin_activate.php
@@ -10,8 +10,22 @@
 			$stmt = $conn->prepare("UPDATE admins	 SET status=:status WHERE id=:id");
 			$stmt->execute(['status'=>1, 'id'=>$id]);
 			$_SESSION['success'] = 'User activated successfully and email sent successfully';
-			$headers = "From: sender\'s email";
-			mail("saadhussain9110@gmail.com", "Very Important Mail", "Milli kay ni oye", $headers);
+			$headers = "From: HydroFarm";
+
+			// Make Query
+
+			// Get Data From Connection Using The Above Data ($id)
+
+			// Parse Email From Data
+			
+			// Store In A New Variable
+			$reciever = "";
+
+
+			mail($reciever, 
+				"Admin Activation Message", 
+				"Your Account With Hydro Farm Has Been Activate By Admin. Now You Can Login. Thanks", 
+				$headers);
 		}
 		catch(PDOException $e){
 			$_SESSION['error'] = $e->getMessage();
